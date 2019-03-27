bcaa757febbfa64be295dde4ec92364174d2733a
Larry Claman <larry.claman@microsoft.com>
2019-03-27 11:21:15 -0400

    end of lab 2

--- lab1.sh
+++ lab1.sh
@@ -23,3 +23,18 @@ az aks browse -g $group -n aks-cluster
 # Lab 2
 ACR_NAME="lncmta032719"
 az acr create -n $ACR_NAME -g $group -l $region --sku Standard --admin-enabled true
+
+# lncmta032719.azurecr.io
+ACR_PWD=`az acr credential show -n $ACR_NAME -g $group --query "passwords[0].value" -o tsv`
+
+kubectl create secret docker-registry acr-auth --docker-server $ACR_NAME.azurecr.io \
+  --docker-username $ACR_NAME --docker-password $ACR_PWD --docker-email ignore@dummy.com
+
+az acr build --registry $ACR_NAME -g $group --file node/data-api/Dockerfile \
+    --image smilr/data-api https://github.com/benc-uk/smilr.git
+az acr build --registry $ACR_NAME -g $group --file node/frontend/Dockerfile \
+    --image smilr/frontend https://github.com/benc-uk/smilr.git
+az acr repository list -g $group --name $ACR_NAME -o table
+
+
+
